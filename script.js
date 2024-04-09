88a56af7b695c6b76059965d0ec99d0fc185007c
nicotambo03 <130360379+nicotambo03@users.noreply.github.com>
2024-04-10 01:13:54 +0200

    .

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function home() {
 
 var seconds = 0;
 var minutes;
+var turni = 1;
 var pausa = false;
 var timerInterval;
 var deciso = false;
@@ -41,9 +42,11 @@ function submit() {
 }
 
 function start() {
-
     var div = document.getElementById('inizio');
     var div2 = document.getElementById('pausa');
+
+    document.getElementById('turni').textContent = turni + '/4';
+    document.getElementById('tomato').style.borderColor = 'tomato';
 
     div.style.display = 'none';
     div2.style.display = 'flex';
@@ -60,7 +63,6 @@ function start() {
     }
 
     timerInterval = setInterval(function () {
-
         document.getElementById('timer').textContent = formatTime(minutes, seconds);
 
         if (pausa == true) {
@@ -75,21 +77,65 @@ function start() {
                 seconds = 59;
             } else {
                 clearInterval(timerInterval);
-                    if (deciso==false) {
-                        minutes = 25;
-                        seconds = 0;
-                        document.getElementById('timer').textContent = formatTime(minutes, seconds);
-                    } else {
-                        document.getElementById('timer').textContent = formatTime(document.getElementById('studio').value, 0);
-                    }
+                if (deciso == false) {
+                    minutes = 25;
+                    seconds = 0;
+                    document.getElementById('timer').textContent = formatTime(minutes, seconds);
+                } else {
+                    document.getElementById('timer').textContent = formatTime(document.getElementById('studio').value, 0);
+                }
                 div.style.display = 'flex';
                 div2.style.display = 'none';
+                turni++;
+                relax();
             }
-        
+        }
+    }, 1000); 
+}
+
+function relax() {
+    var div = document.getElementById('tomato');
+    div.style.borderColor = '#54d16f';
+    document.getElementById('inizio').disabled = true;
+    document.getElementById('stop').disabled = true;
+
+    if (deciso == false) {
+        minutes = 5;
+        seconds = 0;
+    } else{
+        minutes = document.getElementById('tempopausa').value; 
+        seconds = 0;
+    }
+
+
+    timerInterval = setInterval(function () {
+        document.getElementById('timer').textContent = formatTime(minutes, seconds);
+
+        if (seconds > 0) {
+            seconds--;
+        } else {
+            if (minutes > 0) {
+                minutes--;
+                seconds = 59;
+            } else {
+                clearInterval(timerInterval);
+                if (deciso == false) {
+                    minutes = 25;
+                    seconds = 0;
+                    document.getElementById('timer').textContent = formatTime(minutes, seconds);
+                } else {
+                    document.getElementById('timer').textContent = formatTime(document.getElementById('studio').value, 0);
+                    minutes = document.getElementById('studio').value;
+                    seconds=0;
+                }
+                document.getElementById('inizio').disabled = false;
+                document.getElementById('stop').disabled = false;
+                start(); 
+            }
         }
     }, 1000);
-
 }
+
 
 function formatTime(minutes, seconds) {
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
